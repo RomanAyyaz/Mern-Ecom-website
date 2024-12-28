97be51e9acfb1d72248082cac325a0151bd86b8c
Roman Ayyaz <romanayaz7@gmail.com>
2024-12-29 02:24:35 +0500

    Some changes

--- client/src/Components/login/Loginapi.js
+++ client/src/Components/login/Loginapi.js
@@ -1,46 +1,48 @@
-//Signup Api
-export const signupData = async (values) => {
-    const response = await fetch('http://localhost:8000/user/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(values),
-    });
-    
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-};
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL; 
 
-//OTP Api
-export const OtpApi = async(otpdata)=>{
-  let response = await fetch('http://localhost:8000/user/otp',{
-    method:'POST',
-    headers:{
-      'Content-Type':'application/json'
+// Signup API
+export const signupData = async (values) => {
+  const response = await fetch(`${API_BASE_URL}/user/signup`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
-    body:JSON.stringify(otpdata)
-  })
+    body: JSON.stringify(values),
+  });
+  
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
   return response.json();
 };
 
-//Signin Api  
-export const SigninApi = async(values)=>{
-  const response = await fetch('http://localhost:8000/user/signin',{
-    method:'POST',
-    headers:{
-      'Content-Type':'application/json'
+// OTP API
+export const OtpApi = async (otpdata) => {
+  let response = await fetch(`${API_BASE_URL}/user/otp`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
-    body:JSON.stringify(values)
-  })
+    body: JSON.stringify(otpdata),
+  });
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+// Signin API
+export const SigninApi = async (values) => {
+  const response = await fetch(`${API_BASE_URL}/user/signin`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(values),
+  });
   if (!response.ok) {
     const errorResponse = await response.json();
     throw new Error(errorResponse.error || 'Network response was not ok');
   }
   return response.json();
-}
+};
